245c34ab19f19204c1b1a2e6fea09c974e7b2e01
DietLasagna <100707359+DietLasagna@users.noreply.github.com>
2022-04-13 17:05:30 -0500

    initial UI for Log.kt and WeighIn.kt

--- app/src/main/java/com/example/hypertrophy/Log.kt
+++ app/src/main/java/com/example/hypertrophy/Log.kt
@@ -1,5 +1,17 @@
 package com.example.hypertrophy
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chargemap.compose.numberpicker.NumberPicker
+import com.example.hypertrophy.ui.theme.HyperTrophyTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -13,4 +25,114 @@ fun LogScreen(navController: NavHostController) {
         content = {  },
         bottomBar = { BottomBarNavigation(navController = navController) }
     )
+}
+
+@Composable
+fun LogScreenUI() {
+    // TEMP STATE HOLDERS
+    var pickerReps by remember { mutableStateOf(5) }
+    var pickerWeights by remember { mutableStateOf(50) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Current Exercise and Set number
+        Card(
+            modifier = Modifier.padding(16.dp),
+            shape = RectangleShape,
+            backgroundColor = MaterialTheme.colors.surface,
+            border = BorderStroke(2.dp, Color.Black),
+            elevation = 8.dp
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "EXERCISE",
+                    modifier = Modifier.padding(24.dp),
+                    style = MaterialTheme.typography.h4
+                )
+                Text(
+                    text = "SET #",
+                    modifier = Modifier.padding(18.dp),
+                    style = MaterialTheme.typography.h6
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Target reps, editable
+            Text("Reps")
+            NumberPicker(
+                value = pickerReps,
+                onValueChange = { pickerReps = it },
+                dividersColor = MaterialTheme.colors.secondaryVariant,
+                range = 0..250
+            )
+            // Target weight, editable
+            Text("Weight")
+            NumberPicker(
+                value = pickerWeights,
+                onValueChange = { pickerWeights = it },
+                dividersColor = MaterialTheme.colors.secondaryVariant,
+                range = 0..2500
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Option to duplicate set
+            Button(
+                onClick = { /*TODO*/ },
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                Text("Duplicate Set")
+            }
+            // Option to skip set (mark as incomplete)
+            Button(
+                onClick = { /*TODO*/ },
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                Text("Skip Set")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        // Big, slap-able button to mark set as complete
+        Row(
+            modifier = Modifier.padding(58.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxSize(),
+//                elevation = ButtonElevation.elevation(enabled = true, interactionSource = TODO()),
+                shape = MaterialTheme.shapes.medium,
+//                colors = /*TODO*/,
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                Text("DONE")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewLogScreenUI() {
+    HyperTrophyTheme {
+        LogScreenUI()
+    }
 }
--- app/src/main/java/com/example/hypertrophy/WeighIn.kt
+++ app/src/main/java/com/example/hypertrophy/WeighIn.kt
@@ -5,12 +5,301 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chargemap.compose.numberpicker.NumberPicker
+import com.example.hypertrophy.ui.theme.HyperTrophyTheme
 
 @Composable
 fun WeighInScreen(navController: NavHostController) {
     Scaffold(
-        topBar = { TopAppBar( title = { Text(text = "Weigh In") }) },
-        content = {  },
+        topBar = { TopAppBar(title = { Text(text = "Weigh In") }) },
+        content = { },
         bottomBar = { BottomBarNavigation(navController = navController) }
     )
+}
+
+@Composable
+fun WeighInWeight() {
+    //
+}
+
+@Composable
+fun WeighInDiet() {
+    //
+}
+
+@Composable
+fun WeighInMeasurements() {
+    // TEMP STATE HOLDERS
+    var upperArmLeft by remember { mutableStateOf("18.5") }
+    var upperArmRight by remember { mutableStateOf("18.5") }
+    var forearmLeft by remember { mutableStateOf("18.5") }
+    var forearmRight by remember { mutableStateOf("18.5") }
+    var chest by remember { mutableStateOf("54.0") }
+    var thighLeft by remember { mutableStateOf("27.0") }
+    var thighRight by remember { mutableStateOf("27.0") }
+    var calfLeft by remember { mutableStateOf("18.5") }
+    var calfRight by remember { mutableStateOf("18.5") }
+    var waist by remember { mutableStateOf("30.0") }
+    var shoulder by remember { mutableStateOf("18.5") }
+
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Upper Arm",
+                modifier = Modifier.fillMaxWidth(0.33f),
+                textAlign = TextAlign.Left
+            )
+            TextField(
+                value = upperArmLeft,
+                onValueChange = { upperArmLeft = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+                label = { Text("left") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+            TextField(
+                value = upperArmRight,
+                onValueChange = { upperArmRight = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+                label = { Text("right") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Forearm",
+                modifier = Modifier.fillMaxWidth(0.33f),
+                textAlign = TextAlign.Left
+            )
+            TextField(
+                value = forearmLeft,
+                onValueChange = { forearmLeft = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+                label = { Text("left") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+            TextField(
+                value = forearmRight,
+                onValueChange = { forearmRight = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+                label = { Text("right") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Chest",
+                modifier = Modifier.fillMaxWidth(0.33f),
+                textAlign = TextAlign.Left
+            )
+            TextField(
+                value = chest,
+                onValueChange = { chest = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+//                label = { Text("left") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+            Spacer(Modifier.width(96.dp))
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Thigh",
+                modifier = Modifier.fillMaxWidth(0.33f),
+                textAlign = TextAlign.Left
+            )
+            TextField(
+                value = thighLeft,
+                onValueChange = { thighLeft = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+                label = { Text("left") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+            TextField(
+                value = thighRight,
+                onValueChange = { thighRight = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+                label = { Text("right") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Calves",
+                modifier = Modifier.fillMaxWidth(0.33f),
+                textAlign = TextAlign.Left
+            )
+            TextField(
+                value = calfLeft,
+                onValueChange = { calfLeft = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+                label = { Text("left") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+            TextField(
+                value = calfRight,
+                onValueChange = { calfRight = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+                label = { Text("right") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Waist",
+                modifier = Modifier.fillMaxWidth(0.33f),
+                textAlign = TextAlign.Left
+            )
+            TextField(
+                value = waist,
+                onValueChange = { waist = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+//                label = { Text("left") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+            Spacer(Modifier.width(96.dp))
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Shoulder",
+                modifier = Modifier.fillMaxWidth(0.33f),
+                textAlign = TextAlign.Left
+            )
+            TextField(
+                value = shoulder,
+                onValueChange = { shoulder = String.format("%.1f", it) },
+                modifier = Modifier.width(96.dp),
+//                label = { Text("left") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small
+            )
+            Spacer(Modifier.width(96.dp))
+        }
+    }
+}
+
+@Composable
+fun WeighInBodyFat() {
+    // TEMP STATE HOLDERS
+    var pickerPercentInt by remember { mutableStateOf(20) }
+    var pickerPercentDec by remember { mutableStateOf(5) }
+
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Body Fat Percentage")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            NumberPicker(
+                value = pickerPercentInt,
+                onValueChange = { pickerPercentInt = it },
+                dividersColor = MaterialTheme.colors.secondaryVariant,
+                range = 0..99
+            )
+            Text(".")
+            NumberPicker(
+                value = pickerPercentDec,
+                onValueChange = { pickerPercentDec = it },
+                dividersColor = MaterialTheme.colors.secondaryVariant,
+                range = 0..9
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewWeighInMeasurements() {
+    HyperTrophyTheme {
+        WeighInMeasurements()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewWeighInBodyFat() {
+    HyperTrophyTheme {
+        WeighInBodyFat()
+    }
 }
