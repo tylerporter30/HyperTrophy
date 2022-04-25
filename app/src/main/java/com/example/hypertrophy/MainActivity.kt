cf5aab059d66df22409bc4c8a77ea1e2e59a39fd
RunxiangHuang <gale6233@gmail.com>
2022-04-25 16:48:16 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/hypertrophy/MainActivity.kt
+++ app/src/main/java/com/example/hypertrophy/MainActivity.kt
@@ -35,8 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    testHistory(programViewModel = programViewModel)
-                    //SplashScreen()
+                    SplashScreen()
                 }
             }
         }
